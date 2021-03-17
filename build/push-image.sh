385a451ac139f28fe60b2e329738ccb6295e03ab
Per Bergland <per@refapp.se>
2021-03-17 19:16:27 +0100

    push both

--- build/push-image.sh
+++ build/push-image.sh
@@ -2,7 +2,9 @@
 DIRECTORY=$(cd `dirname $0` && pwd)
 VERSION=$(cat "${DIRECTORY}/../VERSION")
 
-for tag in latest ${VERSION}; do 
-    docker tag exchange-ndr-handler:${tag} twrefapp/exchange-ndr-handler:${tag}
-    docker push twrefapp/exchange-ndr-handler:${tag}
+for image in exchange-ndr-handler inbox-notifier; do
+    for tag in latest ${VERSION}; do 
+        docker tag ${image}:${tag} twrefapp/${image}:${tag}
+        docker push twrefapp/${image}:${tag}
+    done
 done
