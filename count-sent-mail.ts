fa6ccec11bf076e910f4192266709958513ad297
Per Bergland <per@refapp.se>
2021-09-17 17:05:13 +0200

    adding script to count sent emails per day

---
+++ count-sent-mail.ts
@@ -0,0 +1,46 @@
+#!/usr/bin/env ts-node-script
+import { SIGUSR1 } from "constants";
+import * as ews from "ews-javascript-api";
+import _ = require("lodash");
+import { withEwsConnection, writeProgress } from "./ews-connect";
+
+async function countSentMail(service: ews.ExchangeService) {
+  const now = new Date();
+  const daysBack = 100;
+  const cutoffDate = new Date(now).setDate(now.getDate() - daysBack);
+  const ewsDateTime = new ews.DateTime(cutoffDate);
+  const filter = new ews.SearchFilter.IsGreaterThan(
+    ews.ItemSchema.DateTimeCreated,
+    ewsDateTime
+  );
+  console.info(`Looking for sent emails after ${ewsDateTime.ToString()}`);
+  let offset = 0;
+  const dates: string[] = [];
+  do {
+    const view = new ews.ItemView(1000, offset);
+    const found = await service.FindItems(
+      ews.WellKnownFolderName.SentItems,
+      filter,
+      view
+    );
+    dates.push(
+      ...found.Items.map((item) => {
+        const sentOn = item.DateTimeSent;
+        return sentOn.Format("yy-MM-DD");
+      })
+    );
+
+    if (!found.MoreAvailable) {
+      break;
+    }
+    offset = found.NextPageOffset;
+  } while (true);
+  const perDay = _.countBy(dates);
+  const sortedPairs = _.toPairs(perDay).sort(([k1], [k2]) => k1.localeCompare(k2));
+  for (const [k,v] of sortedPairs) {
+    console.info(`${k}\t${v}`)
+  }
+}
+
+writeProgress("count-sent-mail");
+withEwsConnection(countSentMail);
