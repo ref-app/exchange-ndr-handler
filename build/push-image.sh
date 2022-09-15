7c7e343bd52f172f8b8c18022b1b731f5d061ac9
Per Bergland <per@refapp.se>
2022-09-15 15:32:49 +0200

    make work

--- build/push-image.sh
+++ build/push-image.sh
@@ -2,5 +2,10 @@
 DIRECTORY=$(cd `dirname $0` && pwd)
 VERSION=$(cat "${DIRECTORY}/../VERSION")
 
-docker buildx build --platform linux/amd64 -t exchange-ndr-handler:${VERSION} --push .
-docker buildx build --platform linux/amd64 -t inbox-notifier:${VERSION} --build-arg SCRIPT="inbox-notification.ts" --push .
+# you may need to run these first:
+# docker buildx create --name mybuilder
+# docker buildx use mybuilder
+# https://cloudolife.com/2022/03/05/Infrastructure-as-Code-IaC/Container/Docker/Docker-buildx-support-multiple-architectures-images/
+
+docker buildx build --push --platform linux/amd64,linux/arm64 -t twrefapp/exchange-ndr-handler:${VERSION} .
+docker buildx build --push --platform linux/amd64,linux/arm64 -t twrefapp/inbox-notifier:${VERSION} --build-arg SCRIPT="inbox-notification.ts" .
