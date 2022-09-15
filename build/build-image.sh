bd398ff9fbed1d0416b432782b6916e49193f556
Per Bergland <per@refapp.se>
2022-09-15 15:20:51 +0200

    use buildx

--- build/build-image.sh
+++ build/build-image.sh
@@ -2,5 +2,5 @@
 DIRECTORY=$(cd `dirname $0` && pwd)
 VERSION=$(cat "${DIRECTORY}/../VERSION")
 
-docker build -t exchange-ndr-handler:latest -t exchange-ndr-handler:${VERSION} . 
-docker build -t inbox-notifier:latest -t inbox-notifier:${VERSION} --build-arg SCRIPT="inbox-notification.ts" . 
+docker buildx build --platform linux/amd64 -t exchange-ndr-handler:latest -t exchange-ndr-handler:${VERSION} .
+docker buildx build --platform linux/amd64 -t inbox-notifier:latest -t inbox-notifier:${VERSION} --build-arg SCRIPT="inbox-notification.ts" .
--- build/push-image.sh
+++ build/push-image.sh
@@ -2,9 +2,5 @@
 DIRECTORY=$(cd `dirname $0` && pwd)
 VERSION=$(cat "${DIRECTORY}/../VERSION")
 
-for image in exchange-ndr-handler inbox-notifier; do
-    for tag in latest ${VERSION}; do 
-        docker tag ${image}:${tag} twrefapp/${image}:${tag}
-        docker push twrefapp/${image}:${tag}
-    done
-done
+docker buildx build --platform linux/amd64 -t exchange-ndr-handler:${VERSION} --push .
+docker buildx build --platform linux/amd64 -t inbox-notifier:${VERSION} --build-arg SCRIPT="inbox-notification.ts" --push .
